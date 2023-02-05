006b48fc3e3a46bc20c61a3a3e9718764d54f216
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-02-05 08:34:45 +0100

    display labels euros and dollars

--- App.java
+++ App.java
@@ -4,7 +4,30 @@ public class App {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello, World.");
+        converter();
+    }
+
+    public static void converter() {
+        JFrame frame = new JFrame("Currency Converter");
+
+        JLabel labelEuro, labelDollar;
+
+        JTextField textFieldEuro, textFieldDollar;
+
+        JButton button1, button2, button3;
+
+        labelEuro = new JLabel("Euros");
+        labelEuro.setBounds(20, 40, 60, 30);
+
+        labelDollar = new JLabel("Dollars");
+        labelDollar.setBounds(20, 40, 60, 30);
+
+        frame.add(labelEuro);
+        frame.add(labelDollar);
+
+        frame.setSize(400, 300);
+        frame.setVisible(true);
+
     }
 
 }
