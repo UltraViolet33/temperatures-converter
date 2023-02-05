173c6f471bb31fa9d0b32b3da0caa363171e176a
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-02-05 08:50:49 +0100

    display btns

--- App.java
+++ App.java
@@ -8,13 +8,15 @@ public class App {
     }
 
     public static void converter() {
+
         JFrame frame = new JFrame("Currency Converter");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JLabel labelEuro, labelDollar;
 
         JTextField textFieldEuro, textFieldDollar;
 
-        JButton button1, button2, button3;
+        JButton buttonEuro, buttonDollar, buttonClose;
 
         labelEuro = new JLabel("Euro");
         labelEuro.setBounds(20, 40, 60, 30);
@@ -22,21 +24,35 @@ public class App {
         labelDollar = new JLabel("Dollars");
         labelDollar.setBounds(170, 40, 60, 30);
         
-        
         textFieldEuro = new JTextField("0");
         textFieldEuro.setBounds(80, 40, 50, 30);
 
-        
         textFieldDollar = new JTextField("0");
         textFieldDollar.setBounds(240, 40, 50, 30);
 
 
+        buttonEuro = new JButton("Euros");
+        buttonEuro.setBounds(50, 80, 80, 15);
+
+        
+        buttonDollar = new JButton("Dollars");
+        buttonDollar.setBounds(190, 80, 80, 15);
+
+        
+        buttonClose = new JButton("Close");
+        buttonClose.setBounds(150, 150, 60, 30);
         
         frame.add(labelEuro);
         frame.add(labelDollar);
 
         frame.add(textFieldEuro);
         frame.add(textFieldDollar);
+
+
+        frame.add(buttonEuro);
+        frame.add(buttonDollar);
+        frame.add(buttonClose);
+
         
         
         frame.setLayout(null);
