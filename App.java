b8750f448cd23d3e3dd6b96f21a4fa8f579e74f9
Ulysse Valdenaire <93035200+UltraViolet33@users.noreply.github.com>
2023-02-05 08:42:28 +0100

    display textFields

--- App.java
+++ App.java
@@ -16,15 +16,30 @@ public class App {
 
         JButton button1, button2, button3;
 
-        labelEuro = new JLabel("Euros");
+        labelEuro = new JLabel("Euro");
         labelEuro.setBounds(20, 40, 60, 30);
 
         labelDollar = new JLabel("Dollars");
-        labelDollar.setBounds(20, 40, 60, 30);
+        labelDollar.setBounds(170, 40, 60, 30);
+        
+        
+        textFieldEuro = new JTextField("0");
+        textFieldEuro.setBounds(80, 40, 50, 30);
 
+        
+        textFieldDollar = new JTextField("0");
+        textFieldDollar.setBounds(240, 40, 50, 30);
+
+
+        
         frame.add(labelEuro);
         frame.add(labelDollar);
 
+        frame.add(textFieldEuro);
+        frame.add(textFieldDollar);
+        
+        
+        frame.setLayout(null);
         frame.setSize(400, 300);
         frame.setVisible(true);
 
